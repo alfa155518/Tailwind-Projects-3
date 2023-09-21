78700636c6632086879d75363d6eaa059b36682b
Ahmed Hassop <alfaelkanas@gmail.com>
2023-09-21 05:53:33 +0300

    Add All Folders

---
+++ tailwind-project-1/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        dim: {
+          50: "#5F99F7",
+          100: "#5F99F7",
+          200: "#38444d",
+          300: "#202e3a",
+          400: "#253341",
+          500: "#5F99F7",
+          600: "#5F99F7",
+          700: "#192734",
+          800: "#162d40",
+          900: "#15202b",
+        },
+      },
+      animation: {
+        "spin-fast": "spin 0.5s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+}
